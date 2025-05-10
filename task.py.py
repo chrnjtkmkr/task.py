4323ab3196866b43a21d7e6930f7dfade9132932
chrnjtkmkr <ck70333980@gmail.com>
2025-05-11 04:57:25 +0530

    Initial commit

---
+++ task.py.py
@@ -0,0 +1,4 @@
+print("welcome to local name generator")
+city = input("which city did you grow up in?\n")
+nickname = input("what is your nickname\n")
+print ("your local name could be: " + city + " " + nickname)
